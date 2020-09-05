6884edfd644178e6fb049e976247e71503188bbf
arsenbelarus <arsenbelarus@gmail.com>
2020-09-05 15:29:05 +0300

    bug fixes cors

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const bodyParser = require("body-parser")
 app.use(cors({
     origin: '*'
 }));
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
