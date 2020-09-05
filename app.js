1b9c3885c3f08d1abe3694f0c81fb2bc2b08bcf0
arsenbelarus <arsenbelarus@gmail.com>
2020-09-05 15:37:11 +0300

    bug fixes cors

--- app.js
+++ app.js
@@ -4,15 +4,7 @@ const nodemailer = require("nodemailer");
 const cors = require("cors")
 const bodyParser = require("body-parser")
 
-app.use(cors({
-    origin: '*'
-}));
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
-    next();
-});
+app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
