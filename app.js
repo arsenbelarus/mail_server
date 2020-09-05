a82e48e70499f83299ab027a6b889cfed2fb9096
arsenbelarus <arsenbelarus@gmail.com>
2020-09-05 15:45:05 +0300

    bug fixes cors

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require("cors")
 const bodyParser = require("body-parser")
 
 const corsOptions = {
-    origin: ['http://localhost:3000/portfolio#contacts', 'https://easytalkchat.netlify.app'],
+    origin: ['http://localhost:3000', 'https://easytalkchat.netlify.app'],
     credentials: true,
 };
 
